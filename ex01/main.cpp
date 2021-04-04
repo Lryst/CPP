1cfea0d81c9099f6f648f04ac8d1f830291a0fae
Lucie RYST <lryst@e1r11p20.42.fr>
2021-04-04 17:14:52 +0200

    cpp00 fini

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lryst <lryst@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 14:48:38 by lryst             #+#    #+#             */
-/*   Updated: 2021/04/03 17:54:02 by lryst            ###   ########.fr       */
+/*   Updated: 2021/04/04 16:38:27 by lryst            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,17 @@ int main()
     std::cout << "-------------------------" << "\n";
     std::cout << "Welcome to my PhoneBook !" << "\n";
     std::cout << "-------------------------" << "\n\n";
-    /* std::cout << "Please enter a command : ";
-    std::cin >> cmd;
-    
-    std::cout << "Command : " << cmd << std::endl; */
     while (1)
     {
         std::cout << "Please enter a command : ";
         std::cin >> cmd;
         
-        //std::cout << "Command : " << cmd << std::endl;
         if (cmd == "ADD")
         {
             std::cin.clear();
             std::cout << "Okey ! Let's add a contact !" << std::endl;
             instance.add_contact();
             std::cout << "Contact created" << std::endl;
-            //instance.print_contact(0);
         }
         else if (cmd == "SEARCH")
         {
