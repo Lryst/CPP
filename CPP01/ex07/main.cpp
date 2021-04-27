54c76302ec4648217e1aed12a6fc747254268e8d
Lucie RYST <lryst@student.42.fr>
2021-04-27 15:32:18 +0200

    cpp01, ex07 ok, CPP01 fini

--- CPP01/ex07/main.cpp
+++ CPP01/ex07/main.cpp
@@ -6,8 +6,48 @@
 /*   By: lryst <lryst@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 18:03:09 by lryst             #+#    #+#             */
-/*   Updated: 2021/04/13 18:07:44 by lryst            ###   ########.fr       */
+/*   Updated: 2021/04/27 15:31:31 by lryst            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Replace.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+int main(int ac, char **av)
+{
+    if (ac == 4)
+    {
+        std::ifstream ifs(av[1]);
+        std::string s1;
+        std::string s2;
+        std::string buff;
+        
+        s1 = av[2];
+        s2 = av[3];
+        if (s1 != "\0" && s2 != "\0")
+        {
+            if (ifs.good())
+            {
+                std::ofstream ofs("FILENAME.replace");
+                while (getline(ifs,buff))
+                {
+                    std::size_t pos = buff.find(s1);
+                    while (pos != std::string::npos)
+                    {
+                        buff.replace(pos, s2.length(),  s2);
+                        pos = buff.find(s1);
+                    }
+                    ofs << buff << std::endl;
+                }
+            }
+        }
+        else
+        {
+            std::cout << "Error, s1 or s2 or both is/are empty." << std::endl;
+        }
+    }
+    else
+        std::cout << "Not the right number of arguments." << std::endl;
+}
